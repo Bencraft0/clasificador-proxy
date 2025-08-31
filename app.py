8bf8dec96967f8816885ea02dfe3a1a4509a763b
Bencraft0 <113908273+Bencraft0@users.noreply.github.com>
2025-08-31 14:12:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
+CORS(app)  # Permite requests desde cualquier origen
 
 # URL de tu Space
 HF_SPACE_URL = "https://huggingface.co/spaces/Bencraft/clasificador-residuo-api/run/predict"
@@ -9,8 +11,8 @@ HF_SPACE_URL = "https://huggingface.co/spaces/Bencraft/clasificador-residuo-api/
 @app.route("/predict", methods=["POST"])
 def proxy_predict():
     try:
-        data = request.json
-        # Enviar al Space
+        data = request.get_json()
+        # Enviar la request al Space
         resp = requests.post(HF_SPACE_URL, json=data, timeout=30)
         resp.raise_for_status()
         return jsonify(resp.json())
@@ -23,4 +25,3 @@ def index():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
